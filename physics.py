75b560aa0269e04d46bfd232f9fa469d3008960a
songz <danielsong1226@gmail.com>
2023-07-17 08:46:22 -0400

    physics/test changes

--- physics.py
+++ physics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 _g = 9.81  # gravity
 _density_water = 1000
 
@@ -29,3 +31,99 @@ def calculate_pressure(h):
         raise Exception("please input the depth as a positive value")
     hydrostatic_pressure = _density_water * _g * h
     return hydrostatic_pressure
+
+
+def calculate_acceleration(F, m):
+    """calculates the acceleration of an object given the force applied to it and its mass"""
+    if m <= 0:
+        raise Exception("please input a positive value for the mass")
+    linear_acceleration = F / m
+    return linear_acceleration
+
+
+def calculate_angular_acceleration(tau, I):
+    """calculates the angular acceleration of an object given the torque applied to it and its moment of inertia"""
+    if I <= 0:
+        raise Exception("please input a positive value for the moment of inertia")
+    angular_acceleration = tau / I
+    return angular_acceleration
+
+
+def calculate_torque(F_magnitude, F_direction, r):
+    """calculates the torque applied to an object given the force applied to it and the distance from the axis of rotation to the point where the force is applied"""
+    if r <= 0:
+        raise Exception("please input a positive value for the radius")
+    torque = F_magnitude * r * np.sin(np.radians(F_direction))
+    return torque
+
+
+def calculate_moment_of_inertia(m, r):
+    """calculates the moment of inertia of an object given its mass and the distance from the axis of rotation to the center of mass of the object"""
+    if r <= 0:
+        raise Exception("please input a positive value for the radius")
+    if m <= 0:
+        raise Exception("please input a positive value for the mass")
+    moment_of_inertia = m * r**2
+    return moment_of_inertia
+
+
+def calculate_auv_acceleration(
+    F_magnitude, F_angle=0, mass=100, volume=0.1, thruster_distance=0.5
+):
+    """return the acceleration of the AUV in meters per second squared"""
+    auv_acceleration = F_magnitude / mass
+    return auv_acceleration
+
+
+def calculate_auv_angular_acceleration(
+    F_magnitude, F_angle, inertia=1, thruster_distance=0.5
+):
+    """return the angular acceleration of the AUV in radians per second squared"""
+    auv_angular_acceleration = (
+        F_magnitude * thruster_distance * np.sin(np.radians(F_angle))
+    ) / inertia
+    return auv_angular_acceleration
+
+
+def calculate_auv2_acceleration(T, alpha, theta, mass=100):
+    """calculates the acceleration of the AUV in the 2D plane"""
+    sin_alpha = np.sin(np.radians(alpha))
+    cos_alpha = np.cos(np.radians(alpha))
+    sin_theta = np.sin(np.radians(theta))
+    cos_theta = np.cos(np.radians(theta))
+    thruster_xy_forces_ratios = np.array(
+        [
+            [cos_alpha, cos_alpha, -cos_alpha, -cos_alpha],
+            [sin_alpha, -sin_alpha, -sin_alpha, sin_alpha],
+        ]
+    )
+    print(thruster_xy_forces_ratios)
+    thrusters_magnitudes = np.array([[T[0]], [T[1]], [T[2]], [T[3]]])
+    print(thrusters_magnitudes)
+    forces_prime = np.dot(thruster_xy_forces_ratios, thrusters_magnitudes)
+    print(forces_prime)
+    rotational_matrix = np.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
+    forces = np.dot(rotational_matrix, forces_prime)
+    auv2_acceleration = np.divide(forces, mass)
+    print(auv2_acceleration)
+
+
+def calculate_auv2_angular_acceleration(T, alpha, L, l, inertia=100):
+    """calculates the angular acceleration of the AUV"""
+    sin_alpha = np.sin(alpha)
+    cos_alpha = np.cos(alpha)
+    radius = np.sqrt(L**2 + l**2)
+    change_to_perpendicular = sin_alpha * L + cos_alpha * l
+    thrusters_magnitudes = np.array([[T[0]], [T[1]], [T[2]], [T[3]]])
+    thrusters_tau = thrusters_magnitudes * radius * change_to_perpendicular
+    total_tau = (
+        thrusters_tau[0] - thrusters_tau[1] + thrusters_tau[2] + thrusters_tau[3]
+    )
+    print(total_tau)
+    auv2_angular_acceleration = total_tau / inertia
+    print(auv2_angular_acceleration)
+
+
+testarray = np.array([1, 3, 1, 2])
+print(calculate_auv2_acceleration(testarray, 70, 40))
+calculate_auv2_angular_acceleration(testarray, 0.5, 1.5, 1.8)
--- test_physics.py
+++ test_physics.py
@@ -37,6 +37,44 @@ class Test_Physics(unittest.TestCase):
         #     self.assertEqual("please input the depth as a positive value", str(e))
         self.assertEqual(physics.calculate_pressure(300), 2943000)
 
+    def test_calculate_acceleration(self):
+        self.assertEqual(physics.calculate_acceleration(500, 10), 50)
+        self.assertEqual(physics.calculate_acceleration(3034, 20), 151.7)
+        try:
+            physics.calculate_acceleration(45, 0)
+        except Exception as e:
+            self.assertEqual(str(e), "please input a positive value for the mass")
+
+    def test_calculate_angular_acceleration(self):
+        self.assertEqual(physics.calculate_angular_acceleration(678, 3), 226)
+        self.assertEqual(physics.calculate_angular_acceleration(300, 20), 15)
+        try:
+            physics.calculate_angular_acceleration(45, -1)
+        except Exception as e:
+            self.assertEqual(
+                str(e), "please input a positive value for the moment of inertia"
+            )
+
+    def test_calculate_torque(self):
+        self.assertEqual(physics.calculate_torque(34, 76, 10), 329.9005469338388)
+        self.assertEqual(physics.calculate_torque(46, 84, 10), 457.4800718694057)
+        try:
+            physics.calculate_torque(34, 76, -1)
+        except Exception as e:
+            self.assertEqual(str(e), "please input a positive value for the radius")
+
+    def test_calculate_moment_of_inertia(self):
+        self.assertEqual(physics.calculate_moment_of_inertia(459, 30), 413100)
+        self.assertEqual(physics.calculate_moment_of_inertia(989, 77), 5863781)
+        try:
+            physics.calculate_moment_of_inertia(459, -5)
+        except Exception as e:
+            self.assertEqual(str(e), "please input a positive value for the radius")
+        try:
+            physics.calculate_moment_of_inertia(0, 77)
+        except Exception as e:
+            self.assertEqual(str(e), "please input a positive value for the mass")
+
 
 if __name__ == "__main__":
     unittest.main()
